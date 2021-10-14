b82a182c026a267e555a7f7448b4f13049b7e012
Václav Pfleger <58568137+vacexpfleger@users.noreply.github.com>
2021-10-14 12:55:22 +0200

    Update test.sh
    deleted a while loop

--- test.sh
+++ test.sh
@@ -1,9 +1,7 @@
 arr=("Postavil jsem sve dceri barak" "Doufam ze umis Cisco" "Sleduji te" "Dneska budeme delat Dockery" "Tak si trosku zatancime" "Sed rovne!" "Ukazeme si routovani" "Postavil jsem sve dceri barak, co ty?" "Černí smrdí jinak" "Byl jsem v Americe a Singapuru" "Choď rovně!" "Big Blue Button" "sleduji te :)" "Jsi na správném operačním systému")
 min=0
 max=14
-while true
-do
     number=$(expr $min + $RANDOM % $max)
     notify-send "Petr Grussmann" "${arr[$number]}" --icon="$HOME/gruss.jpg"
     sleep 1500s
-done
+exit
