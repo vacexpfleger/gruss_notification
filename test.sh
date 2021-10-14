f58f6f38226761927f42d6bef6402675e3d7415f
Václav Pfleger <58568137+vacexpfleger@users.noreply.github.com>
2021-10-14 13:23:18 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,6 @@
 arr=("Postavil jsem sve dceri barak" "Doufam ze umis Cisco" "Sleduji te" "Dneska budeme delat Dockery" "Tak si trosku zatancime" "Sed rovne!" "Ukazeme si routovani" "Postavil jsem sve dceri barak, co ty?" "Černí smrdí jinak" "Byl jsem v Americe a Singapuru" "Choď rovně!" "Big Blue Button" "sleduji te :)" "Jsi na správném operačním systému")
 min=0
 max=14
-do
-    number=$(expr $min + $RANDOM % $max)
-    notify-send "Petr Grussmann" "${arr[$number]}" --icon="icon.jpg"
-done
+number=$(expr $min + $RANDOM % $max)
+notify-send "Petr Grussmann" "${arr[$number]}" --icon="icon.jpg"
 exit
